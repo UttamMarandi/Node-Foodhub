542b335147601421e99a8bcca27df62981db0b40
Uttam Marandi <uttammarandi007@gmail.com>
2022-01-23 10:43:02 +0530

    register user - controller

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,25 @@
+const User = require("../models/User");
+const passport = require("passport");
+const bcrypt = require("bcryptjs");
+
 const asyncWrapper = require("../middleware/async");
 
 const registerUser = asyncWrapper(async (req, res) => {
-  res.status(200).json({ msg: "User Registered" });
+  User.findOne({ email: req.body.email }, async (err, doc) => {
+    // doc is the document which satisfies the filter
+    if (err) {
+      return res.status(400).json({ msg: "Error in registerUser 1st block" });
+    }
+    if (doc) {
+      return res.status(400).json({ msg: "User already exists" });
+    }
+    if (!doc) {
+      const hashedPassword = await bcrypt.hash(req.body.password, 12);
+      const newUser = { ...req.body, password: hashedPassword };
+      const savedUser = await User.create(newUser);
+      return res.status(200).json({ savedUser });
+    }
+  });
 });
 
 const loginUser = asyncWrapper(async (req, res) => {
